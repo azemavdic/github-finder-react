3f949c6b646503cd1538bd5ee72ec57971d965d4
unknown <asko2212@hotmail.com>
2021-12-10 10:25:03 +0100

    reset search

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,20 +1,20 @@
-import { useContext } from "react";
-import GithubContext from "../../context/github/GithubContext";
-import Spinner from "../Layout/Spinner";
-import UserItem from "./UserItem";
+import { useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
+import Spinner from '../Layout/Spinner';
+import UserItem from './UserItem';
 
 const UserResults = () => {
-  const { users, loading } = useContext(GithubContext);
+    const { users, loading } = useContext(GithubContext);
 
-  return loading ? (
-    <Spinner />
-  ) : (
-    <div className='grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-8'>
-      {users.map((user) => (
-        <UserItem key={user.id} user={user} />
-      ))}
-    </div>
-  );
+    return loading ? (
+        <Spinner />
+    ) : (
+        <div className='grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-8'>
+            {users.map((user) => (
+                <UserItem key={user.id} user={user} />
+            ))}
+        </div>
+    );
 };
 
 export default UserResults;
--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,40 +1,54 @@
-import { createContext, useReducer } from "react";
-import githubReducer from "./GithubReducer";
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
 export const GithubProvider = ({ children }) => {
-  const initialState = {
-    users: [],
-    loading: false,
-  };
+    const initialState = {
+        users: [],
+        loading: false,
+    };
 
-  const [state, dispatch] = useReducer(githubReducer, initialState);
+    const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
-    setLoading();
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await res.json();
+    const searchUsers = async (text) => {
+        setLoading();
 
-    dispatch({
-      type: "GET_USERS",
-      payload: data,
-    });
-  };
+        const params = new URLSearchParams({
+            q: text,
+        });
 
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
+        const res = await fetch(
+            `${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,
+            {
+                //   headers: {
+                //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+                //   },
+            }
+        );
+        const { items } = await res.json();
+        dispatch({
+            type: 'GET_USERS',
+            payload: items,
+        });
+    };
 
-  return (
-    <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
-    >
-      {children}
-    </GithubContext.Provider>
-  );
+    const resetSearch = () => dispatch({ type: 'RESET_SEARCH' });
+
+    const setLoading = () => dispatch({ type: 'SET_LOADING' });
+
+    return (
+        <GithubContext.Provider
+            value={{
+                users: state.users,
+                loading: state.loading,
+                searchUsers,
+                resetSearch,
+            }}
+        >
+            {children}
+        </GithubContext.Provider>
+    );
 };
 
 export default GithubContext;
