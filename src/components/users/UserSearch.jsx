ef69f339e1dea4ab8cede7a9f82243ae55d32178
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-12-11 22:27:33 +0100

    user profile

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,58 +1,57 @@
-import { useState, useContext } from 'react';
-import GithubContext from '../../context/github/GithubContext';
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 const UserSearch = () => {
-    const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-    const { users, searchUsers, resetSearch } = useContext(GithubContext);
+  const { users, searchUsers, resetSearch } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if (text === '') {
-            alert('Upisite nesto u pretragu');
-        } else {
-            //Pretraga korisnika
-            searchUsers(text);
-        }
-        setText('');
-    };
+    if (text === "") {
+      setAlert("Upisite nesto u pretragu.", "error");
+    } else {
+      //Pretraga korisnika
+      searchUsers(text);
+    }
+    setText("");
+  };
 
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 mb-8 gap-8'>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <div className='form-control'>
-                        <div className='relative'>
-                            <input
-                                type='text'
-                                className='w-full input text-black bg-gray-200 input-lg pr-40'
-                                placeholder='Pretraga'
-                                value={text}
-                                onChange={(e) => setText(e.target.value)}
-                            />
-                            <button
-                                type='submit'
-                                className='btn btn-lg absolute top-0 right-0 rounded-l-none'
-                            >
-                                Pretraži
-                            </button>
-                        </div>
-                    </div>
-                </form>
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 mb-8 gap-8'>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <div className='relative'>
+              <input
+                type='text'
+                className='w-full input text-black bg-gray-200 input-lg pr-40'
+                placeholder='Pretraga'
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+              <button
+                type='submit'
+                className='btn btn-lg absolute top-0 right-0 rounded-l-none'
+              >
+                Pretraži
+              </button>
             </div>
-            {users.length > 0 && (
-                <div>
-                    <button
-                        className='btn btn-ghost btn-lg'
-                        onClick={resetSearch}
-                    >
-                        Reset
-                    </button>
-                </div>
-            )}
+          </div>
+        </form>
+      </div>
+      {users.length > 0 && (
+        <div>
+          <button className='btn btn-ghost btn-lg' onClick={resetSearch}>
+            Reset
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default UserSearch;
--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,54 +1,82 @@
-import { createContext, useReducer } from 'react';
-import githubReducer from './GithubReducer';
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
 export const GithubProvider = ({ children }) => {
-    const initialState = {
-        users: [],
-        loading: false,
-    };
+  const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+  };
 
-    const [state, dispatch] = useReducer(githubReducer, initialState);
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
-    const searchUsers = async (text) => {
-        setLoading();
+  //Pretraga korisnika na input
+  const searchUsers = async (text) => {
+    setLoading();
 
-        const params = new URLSearchParams({
-            q: text,
-        });
+    const params = new URLSearchParams({
+      q: text,
+    });
 
-        const res = await fetch(
-            `${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,
-            {
-                //   headers: {
-                //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-                //   },
-            }
-        );
-        const { items } = await res.json();
-        dispatch({
-            type: 'GET_USERS',
-            payload: items,
-        });
-    };
-
-    const resetSearch = () => dispatch({ type: 'RESET_SEARCH' });
-
-    const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
-    return (
-        <GithubContext.Provider
-            value={{
-                users: state.users,
-                loading: state.loading,
-                searchUsers,
-                resetSearch,
-            }}
-        >
-            {children}
-        </GithubContext.Provider>
+    const res = await fetch(
+      `${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
     );
+    const { items } = await res.json();
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
+  //Profil korisnika
+  const getUser = async (login) => {
+    setLoading();
+
+    const res = await fetch(
+      `${process.env.REACT_APP_GITHUB_URL}/users/${login}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    if (res.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await res.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
+
+  const resetSearch = () => dispatch({ type: "RESET_SEARCH" });
+
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,
+        searchUsers,
+        resetSearch,
+        getUser,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
 };
 
 export default GithubContext;
